fe2d719020496765b56fa25993be8c62d2c81436
Stefan Boehringer <github@s-boehringer.org>
2013-05-02 15:28:26 +0200

    program logic scaffold

---
+++ meta_queueLogic.pm
@@ -0,0 +1,25 @@
+#
+#	meta_queueLogic.pm
+#Thu May  2 15:01:57 CEST 2013
+
+use MooseX::Declare;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods;
+
+class My::Schema {
+
+}
+
+class meta_queueQGS {
+	use TempFileNames;
+	use Set;
+	use Data::Dumper;
+	use POSIX qw(strftime mktime);
+	use POSIX::strptime qw(strptime);
+	use utf8;
+
+	method greetings() {
+		Log("Hello world");
+	}
+
+}
